4dbe1b6f20acceab802f551d9b332e312cea3b1d
Zhi <yfwz100@yeah.net>
2016-04-10 10:47:42 +0800

    The final AC answer.

--- src/microsoft/p201604/p4/Main.java
+++ src/microsoft/p201604/p4/Main.java
@@ -9,8 +9,8 @@ import java.util.*;
  * 1. The newly placed cube must be adjacent to the ground or a previously placed cube. Two cubes are adjacent if and
  * only if they share a same face.
  * <p>
- * 2. The newly placed cube must be accessible from outside which means by moving in 6 directions(up, down, left, right,
- * forward, far) there is a path from a very far place - say (1000, 1000, 1000) in this problem - to this cube
+ * 2. The newly placed cube must be accessible from outside which means by moving in 6 directions(up, down, minX, maxX,
+ * forward, maxY) there is a path from a very maxY build - say (1000, 1000, 1000) in this problem - to this cube
  * without passing through ground or other cubes.
  * <p>
  * Given a sequence of cubes Little Hi wants to know if he can build the world by placing the cubes in such order.
@@ -19,67 +19,133 @@ import java.util.*;
  */
 public class Main {
 
+    /**
+     * The maze interface.
+     */
     interface Maze {
-        boolean place(int x, int y, int z);
+        /**
+         * for building the maze.
+         *
+         * @param x the x coordinate.
+         * @param y the y coordinate.
+         * @param z the z coordinate.
+         * @return true if it can be built.
+         */
+        boolean build(int x, int y, int z);
+
+        /**
+         * To destroy the building.
+         *
+         * @return true if successful.
+         */
+        boolean unbuild();
     }
 
     private static class ArrayMaze implements Maze {
+        private final static int SIZE = 101;
+        private final static int[][] DIR = {
+            {-1, 0, 0}, {0, -1, 0}, {0, 0, -1},
+            {1, 0, 0}, {0, 1, 0}, {0, 0, 1}
+        };
 
-        int[][][] cubes = new int[101][101][101];
+        int[][][] cubes = new int[SIZE][SIZE][SIZE];
+        int[][][] visited = new int[SIZE][SIZE][SIZE];
 
-        int right;
-        int far;
-        int upper;
+        int maxX, minX = Integer.MAX_VALUE;
+        int maxY, minY = Integer.MAX_VALUE;
+        int maxZ, minZ = Integer.MAX_VALUE;
 
-        boolean reachable(int x, int y, int z) {
-            // bfs
-            Set<Integer> visited = new HashSet<>();
-            Deque<Integer> uxs = new ArrayDeque<>(), uys = new ArrayDeque<>(), uzs = new ArrayDeque<>();
-            uxs.add(x); uys.add(y); uzs.add(z);
-            while (!uxs.isEmpty()) {
-                int cx = uxs.poll(), cy = uys.poll(), cz = uzs.poll();
-                if (cx < 0 || cy < 0 || cx > right || cy > far || cz > upper) {
-                    return true;
+        private class Coord {
+            int x, y, z;
+
+            Coord(int x, int y, int z) {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+        }
+
+        private Deque<Coord> points = new ArrayDeque<>();
+
+        ArrayMaze() {
+            // fill the solid ground.
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    cubes[i][j][0] = 1;
                 }
-                int idx = cx * 10000 + cy * 100 + cz;
-                if (!visited.contains(idx) && cubes[cx][cy][cz] == 0) {
-                    if (cz > 1) {
-                        uxs.add(cx); uys.add(cy); uzs.add(cz - 1);
-                    }
-                    uxs.add(cx - 1); uys.add(cy); uzs.add(cz);
-                    uxs.add(cx + 1); uys.add(cy); uzs.add(cz);
-                    uxs.add(cx); uys.add(cy - 1); uzs.add(cz);
-                    uxs.add(cx); uys.add(cy + 1); uzs.add(cz);
-                    uxs.add(cx); uys.add(cy); uzs.add(cz + 1);
+            }
+        }
 
-                    visited.add(idx);
+        private boolean attachable(int x, int y, int z) {
+            for (int[] dir:DIR) {
+                if (cubes[x + dir[0]][y + dir[1]][z + dir[2]] > 0) {
+                    return true;
                 }
             }
             return false;
         }
 
-        boolean test(int x, int y, int z) {
-            return x > 0 && y > 0 && z > 0 && cubes[x][y][z] > 0;
-        }
-
-        boolean attachable(int x, int y, int z) {
-            return z == 1
-                    || test(x - 1, y, z) || test(x + 1, y, z)
-                    || test(x, y + 1, z) || test(x, y - 1, z)
-                    || test(x, y, z - 1) || test(x, y, z + 1);
-        }
-
-        public boolean place(int x, int y, int z) {
-            if (attachable(x, y, z) && reachable(x, y, z)) {
-                right = Math.max(x, right);
-                far = Math.max(y, far);
-                upper = Math.max(z, upper);
+        public boolean build(int x, int y, int z) {
+            if (cubes[x][y][z] == 0 && attachable(x, y, z)) {
+                // visit the cube.
                 cubes[x][y][z] = 1;
+                // set the bounds
+                minX = Math.min(x, minX);
+                maxX = Math.max(x, maxX);
+                minY = Math.min(y, minY);
+                maxY = Math.max(y, maxY);
+                minZ = Math.min(z, minZ);
+                maxZ = Math.max(z, maxZ);
+                // push back for destructing.
+                points.add(new Coord(x, y, z));
                 return true;
             } else {
                 return false;
             }
         }
+
+        private boolean bfs(Coord p, int n) {
+            boolean ret = false;
+            Queue<Coord> q = new ArrayDeque<>();
+            q.add(p);
+            visited[p.x][p.y][p.z] = n;
+            while (!q.isEmpty()) {
+                Coord c = q.poll();
+                if (c.x < minX || c.y < minY || c.z < minZ || c.x > maxX || c.y > maxY || c.z > maxZ) {
+                    ret = true;
+                    break;
+                }
+                for (int[] dir : DIR) {
+                    int nx = c.x + dir[0];
+                    int ny = c.y + dir[1];
+                    int nz = c.z + dir[2];
+                    if (cubes[nx][ny][nz] == 0 && n != visited[nx][ny][nz]) {
+                        if (visited[nx][ny][nz] > 0) {
+                            ret = true;
+                            break;
+                        } else {
+                            visited[nx][ny][nz] = n;
+                            q.add(new Coord(nx, ny, nz));
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+
+        @Override
+        public boolean unbuild() {
+            while (!points.isEmpty()) {
+                Coord p = points.pollLast();
+                // unbuild.
+                cubes[p.x][p.y][p.z] = 0;
+                if (!bfs(p, points.size() + 1)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
     }
 
     public static void main(String... args) {
@@ -90,24 +156,16 @@ public class Main {
                 int voxels = sc.nextInt();
                 Maze maze = new ArrayMaze();
                 boolean flag = true;
-                int j;
-                for (j = 0; j < voxels; j++) {
+                for (int j = 0; j < voxels; j++) {
                     int x = sc.nextInt(), y = sc.nextInt(), z = sc.nextInt();
-                    if (!maze.place(x, y, z)) {
+                    if (flag && !maze.build(x, y, z)) {
                         flag = false;
-//                        System.out.println(x + "," + y + "," + z);
-                        break;
                     }
                 }
-                if (flag) {
+                if (flag && maze.unbuild()) {
                     System.out.println("Yes");
                 } else {
                     System.out.println("No");
-                }
-                for (; j < voxels-1; j++) {
-                    sc.nextInt();
-                    sc.nextInt();
-                    sc.nextInt();
                 }
             }
         }
